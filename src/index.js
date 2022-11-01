2e4b4dfbb51db217d874558a86998236199d3dc9
Gil Jacob Abadi <giljabadi@gmail.com>
2022-11-01 14:06:40 -0500

    complete card exercise from react course

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-// 1) Import the react and react dom libraries
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { faker } from "@faker-js/faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCards from "./ApprovalCards";
+import '/Users/gilabadi/jsx/src/App.css';
 
-// 2) Get a recerence to the div with id root
-const el = document.getElementById('root');
-// 3) Tell React to take control of that element
-const root = ReactDOM.createRoot(el);
-// 4) Create a component
-function App() {
-        return <textarea autoFocus={true}></textarea>
-}
-// 5) Show the component on the screen
-root.render(<App/>);
+//////////////////////////////////////////////////
 
+const App =() => {
+        return (
+          <div className="ui container comments">
+              <ApprovalCards>
+                  <div>
+                      <h4>Warning!</h4>
+                      Are you sure?
+                  </div>
+              </ApprovalCards>
+
+              <ApprovalCards>
+                <CommentDetail
+                author = "Sam"
+                timeAgo = "Today at 4:54PM"
+                avatar = {faker.image.avatar()}
+                post = "hahahah"
+                />
+              </ApprovalCards>
+              <ApprovalCards>
+                  <CommentDetail
+                      author = "Alex"
+                      timeAgo = "Today at 1:24PM"
+                      avatar = {faker.image.avatar()}
+                      post = "LMAO"
+                  />
+              </ApprovalCards>
+
+              <ApprovalCards>
+                  <CommentDetail
+                      author = "Jane"
+                      timeAgo = "Yesterday at 2:16AM"
+                      avatar = {faker.image.avatar()}
+                      post = "test"
+                  />
+              </ApprovalCards>
+          </div>
+
+        )
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+/*
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+/>
+
+
+
+ */
